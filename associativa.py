7d6097e73da850c7dd8a63fdb4bac4ff8a3a6b0c
Leandra Coutinho <leandracou123@gmail.com>
2026-03-02 09:03:08 -0300

    add graphics

--- associativa.py
+++ associativa.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def convolucao(x, h):
     """
@@ -8,10 +9,7 @@ def convolucao(x, h):
     n = len(x)
     m = len(h)
     
-    # tamanho da convolução
     tamanho_saida = n + m - 1
-    
-    # inicializa com zeros
     y = [0] * tamanho_saida
     
     for i in range(n):
@@ -29,11 +27,6 @@ x = [1, 2, 3]
 h = [0, 1, 0.5]
 g = [2, -1]
 
-
-#x = gerar_sequencia(4)
-#h = gerar_sequencia(3)
-#g = gerar_sequencia(2)
-
 print("x =", x)
 print("h =", h)
 print("g =", g)
@@ -46,5 +39,27 @@ lado2 = convolucao(x, convolucao(h, g))
 
 print("\n(x * h) * g =", lado1)
 print("x * (h * g) =", lado2)
-
 print("\nSão iguais?", lado1 == lado2)
+
+
+# ==========================
+# GERAÇÃO DOS GRÁFICOS
+# ==========================
+
+# Gráfico do lado 1
+plt.figure()
+plt.stem(range(len(lado1)), lado1)
+plt.title("(x * h) * g")
+plt.xlabel("n")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
+
+# Gráfico do lado 2
+plt.figure()
+plt.stem(range(len(lado2)), lado2)
+plt.title("x * (h * g)")
+plt.xlabel("n")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
